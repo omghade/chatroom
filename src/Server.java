430dd7aab9406b4c9789f361cf7c112bea939b1a
omghade <ghade606@gmail.com>
2024-08-16 11:11:48 -0700

    server sreated

---
+++ src/Server.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server implements Runnable{
+    private ArrayList<ConnectionHandler> connections;
+
+    @Override
+    public void run() {
+        try {
+            ServerSocket server = new ServerSocket(9999);
+            Socket client = server.accept();
+        } catch (IOException e) {
+            // TODO: handle
+        }
+    }
+
+    class connectionHandler implements Runnable {
+        private Socket client;
+        private BufferedReader in;
+        private PrintWriter out;
+        private String nickname;
+
+        public connectionHandler(Socket client) {
+            this.client = client;
+        }
+        @Override
+        public void run () {
+            try{
+                out = new PrintWriter(client.getOutputStream(),true);
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                out.println("Please enter a nickname : ");
+                nickname = in.readLine();
+                System.out.println(nickname + " connected!");
+            }catch (IOException e) {
+                // TODO: handel
+            }
+        }
+    }
+
+}
